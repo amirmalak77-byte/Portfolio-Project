910dac3419b6da81cab0e65b89ca453e9d2d4c3c
Amir Malak <amirmalak77@gmail.com>
2025-12-11 16:24:51 +0200

    Portfolio

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+const menuIcon = document.querySelector(".nav-menu");
+const navLinks = document.querySelector(".holder-links");
+const closeIcon = document.querySelector(".close-icon");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.add("open");
+});
+closeIcon.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+});
+
+const linkItems = document.getElementsByClassName("link-item");
+
+Array.from(linkItems).forEach((link) => {
+  link.addEventListener("click", () => {
+    Array.from(linkItems).forEach((item) => {
+      item.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
+
+console.log(linkItems);
